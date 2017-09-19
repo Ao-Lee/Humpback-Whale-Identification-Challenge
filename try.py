354d991a59c0b16fbfb279cc0b72c35bc9ca03bc
Ao-Lee <ao-li-2008@hotmail.com>
2017-09-19 09:05:20 +0800

    data inputs.

--- try.py
+++ try.py
@@ -2,9 +2,15 @@ from collections import defaultdict
 import pandas as pd
 import numpy as np
 from sklearn.cross_validation import train_test_split
+from sklearn.preprocessing import normalize
 from os.path import join
 from PIL import Image
-from keras.applications.resnet50 import preprocess_input
+from keras import backend as K
+from keras.models import Model
+from keras.optimizers import Adam
+from keras.layers import Input, Dense, Dropout, Lambda
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+from keras.applications.resnet50 import ResNet50, preprocess_input
 import matplotlib.pyplot as plt
 
 batch_size = 24
@@ -72,6 +78,10 @@ def gen(triplet_gen):
             negative_example_img = read_and_resize(path_neg)
             positive_example_2_img = read_and_resize(path_pos2)
 
+            positive_example_1_img = augment(positive_example_1_img)
+            negative_example_img = augment(negative_example_img)
+            positive_example_2_img = augment(positive_example_2_img)
+            
             list_positive_examples_1.append(positive_example_1_img)
             list_negative_examples.append(negative_example_img)
             list_positive_examples_2.append(positive_example_2_img)
@@ -84,7 +94,67 @@ def gen(triplet_gen):
         
         yield ({'anchor_input': A, 'positive_input': B, 'negative_input': C}, label)
 
+def triplet_loss(inputs, dist='sqeuclidean', margin='maxplus'):
+    anchor, positive, negative = inputs
+    positive_distance = K.square(anchor - positive)
+    negative_distance = K.square(anchor - negative)
+    if dist == 'euclidean':
+        positive_distance = K.sqrt(K.sum(positive_distance, axis=-1, keepdims=True))
+        negative_distance = K.sqrt(K.sum(negative_distance, axis=-1, keepdims=True))
+    elif dist == 'sqeuclidean':
+        positive_distance = K.sum(positive_distance, axis=-1, keepdims=True)
+        negative_distance = K.sum(negative_distance, axis=-1, keepdims=True)
+    loss = positive_distance - negative_distance
+    if margin == 'maxplus':
+        loss = K.maximum(0.0, 1 + loss)
+    elif margin == 'softplus':
+        loss = K.log(1 + K.exp(loss))
+    return K.mean(loss)
 
+def check_loss():
+    batch_size = 10
+    shape = (batch_size, 4096)
+
+    p1 = normalize(np.random.random(shape))
+    n = normalize(np.random.random(shape))
+    p2 = normalize(np.random.random(shape))
+    
+    input_tensor = [K.variable(p1), K.variable(n), K.variable(p2)]
+    out1 = K.eval(triplet_loss(input_tensor))
+    input_np = [p1, n, p2]
+    out2 = triplet_loss_np(input_np)
+
+    assert out1.shape == out2.shape
+    print(np.linalg.norm(out1))
+    print(np.linalg.norm(out2))
+    print(np.linalg.norm(out1-out2))
+    
+def GetModel():
+    base_model = ResNet50(weights='imagenet', include_top=False, pooling='max')
+    for layer in base_model.layers:
+        layer.trainable = False
+    
+    x = base_model.output
+    x = Dropout(0.5)(x)
+    x = Dense(embedding_dim)(x)
+    x = Lambda(lambda  x: K.l2_normalize(x,axis=1))(x)
+    embedding_model = Model(base_model.input, x, name="embedding")
+
+    input_shape = (image_size, image_size, 3)
+    anchor_input = Input(input_shape, name='anchor_input')
+    positive_input = Input(input_shape, name='positive_input')
+    negative_input = Input(input_shape, name='negative_input')
+    anchor_embedding = embedding_model(anchor_input)
+    positive_embedding = embedding_model(positive_input)
+    negative_embedding = embedding_model(negative_input)
+
+    inputs = [anchor_input, positive_input, negative_input]
+    outputs = [anchor_embedding, positive_embedding, negative_embedding]
+       
+    triplet_model = Model(inputs, outputs)
+    triplet_model.add_loss(K.mean(triplet_loss(outputs)))
+
+    return embedding_model, triplet_model
 
 data = pd.read_csv(path_csv)
 train, test = train_test_split(data, train_size=0.7, random_state=1337)
@@ -92,6 +162,10 @@ file_id_mapping_train = {k: v for k, v in zip(train.Image.values, train.Id.value
 file_id_mapping_test = {k: v for k, v in zip(test.Image.values, test.Id.values)}
 gen_tr = gen(sample_gen(file_id_mapping_train))
 gen_te = gen(sample_gen(file_id_mapping_test))
+
+checkpoint = ModelCheckpoint(path_model, monitor='loss', verbose=1, save_best_only=True, mode='min')
+early = EarlyStopping(monitor="val_loss", mode="min", patience=2)
+callbacks_list = [checkpoint, early]  # early
 
 def ShowImg(img):
     plt.figure()
@@ -109,3 +183,38 @@ img[..., 1] += mean[1]
 img[..., 2] += mean[2]
 img = img[..., ::-1]
 ShowImg(img)
+
+embedding_model, triplet_model = GetModel()
+
+for layer in embedding_model.layers[175:]:
+    layer.trainable = True
+for layer in embedding_model.layers[:175]:
+    layer.trainable = False
+    
+triplet_model.compile(loss=None, optimizer=Adam(0.01))
+
+history = triplet_model.fit_generator(gen_tr, 
+                              validation_data=gen_te, 
+                              epochs=3, 
+                              verbose=1, 
+                              workers=4,
+                              steps_per_epoch=200, 
+                              validation_steps=20)
+
+for layer in embedding_model.layers[153:]:
+    layer.trainable = True
+for layer in embedding_model.layers[:153]:
+    layer.trainable = False
+triplet_model.compile(loss=None, optimizer=Adam(0.0001))
+
+history = triplet_model.fit_generator(gen_tr, 
+                                    validation_data=gen_te, 
+                                    epochs=3, 
+                                    verbose=1, 
+                                    workers=4,
+                                    steps_per_epoch=60, 
+                                    validation_steps=30)
+
+plt.plot(history.history['loss'], label='loss')
+plt.legend()
+plt.show()
