f3a51b1af22a0626687cd58d31082a9bf5b6e6e7
Ao-Lee <ao-li-2008@hotmail.com>
2017-10-06 16:45:28 +0800

    large dataset supported

--- try.py
+++ try.py
@@ -1,17 +1,16 @@
-from collections import defaultdict
 import pandas as pd
 import numpy as np
 from sklearn.cross_validation import train_test_split
 from sklearn.preprocessing import normalize
 from os.path import join
-from PIL import Image
 from keras import backend as K
 from keras.models import Model
 from keras.optimizers import Adam
 from keras.layers import Input, Dense, Dropout, Lambda
 from keras.callbacks import ModelCheckpoint, EarlyStopping
-from keras.applications.resnet50 import ResNet50, preprocess_input
+from keras.applications.resnet50 import ResNet50
 import matplotlib.pyplot as plt
+from data import gen, sample_gen
 
 batch_size = 24
 embedding_dim = 50
@@ -21,83 +20,6 @@ path_train = join(path_base,'Data', 'train')
 path_test = join(path_base,'Data', 'test')
 path_model = join(path_base,'Model', 'MyModel.hdf5')
 path_csv = 'E:\\DM\\Humpback Whale Identification Challenge\\Data\\train.csv'
-
-class sample_gen(object):
-    def __init__(self, file_class_mapping, other_class = "new_whale"):
-        self.file_class_mapping= file_class_mapping
-        self.class_to_list_files = defaultdict(list)
-        self.list_other_class = []
-        self.list_all_files = list(file_class_mapping.keys())
-        self.range_all_files = list(range(len(self.list_all_files)))
-
-        for file, class_ in file_class_mapping.items():
-            if class_ == other_class:
-                self.list_other_class.append(file)
-            else:
-                self.class_to_list_files[class_].append(file)
-
-        self.list_classes = list(set(self.file_class_mapping.values()))
-        self.range_list_classes= range(len(self.list_classes))
-        self.class_weight = np.array([len(self.class_to_list_files[class_]) for class_ in self.list_classes])
-        self.class_weight = self.class_weight/np.sum(self.class_weight)
-
-    def get_sample(self):
-        class_idx = np.random.choice(self.range_list_classes, 1, p=self.class_weight)[0]
-        examples_class_idx = np.random.choice(range(len(self.class_to_list_files[self.list_classes[class_idx]])), 2)
-        positive_example_1, positive_example_2 = \
-            self.class_to_list_files[self.list_classes[class_idx]][examples_class_idx[0]],\
-            self.class_to_list_files[self.list_classes[class_idx]][examples_class_idx[1]]
-
-
-        negative_example = None
-        while negative_example is None or self.file_class_mapping[negative_example] == \
-                self.file_class_mapping[positive_example_1]:
-            negative_example_idx = np.random.choice(self.range_all_files, 1)[0]
-            negative_example = self.list_all_files[negative_example_idx]
-        return positive_example_1, negative_example, positive_example_2
-    
-def read_and_resize(filepath):
-    im = Image.open((filepath)).convert('RGB')
-    im = im.resize((image_size, image_size))
-    return np.array(im, dtype="float32")
-
-
-def augment(im_array):
-    if np.random.uniform(0, 1) > 0.9:
-        im_array = np.fliplr(im_array)
-    return im_array
-
-def gen(triplet_gen):
-    while True:
-        list_positive_examples_1 = []
-        list_negative_examples = []
-        list_positive_examples_2 = []
-
-        for i in range(batch_size):
-            positive_example_1, negative_example, positive_example_2 = triplet_gen.get_sample()
-            path_pos1 = join(path_train, positive_example_1)
-            path_neg = join(path_train, negative_example)
-            path_pos2 = join(path_train, positive_example_2)
-            
-            positive_example_1_img = read_and_resize(path_pos1)
-            negative_example_img = read_and_resize(path_neg)
-            positive_example_2_img = read_and_resize(path_pos2)
-
-            positive_example_1_img = augment(positive_example_1_img)
-            negative_example_img = augment(negative_example_img)
-            positive_example_2_img = augment(positive_example_2_img)
-            
-            list_positive_examples_1.append(positive_example_1_img)
-            list_negative_examples.append(negative_example_img)
-            list_positive_examples_2.append(positive_example_2_img)
-
-        A = preprocess_input(np.array(list_positive_examples_1))
-        B = preprocess_input(np.array(list_positive_examples_2))
-        C = preprocess_input(np.array(list_negative_examples))
-        
-        label = None
-        
-        yield ({'anchor_input': A, 'positive_input': B, 'negative_input': C}, label)
 
 def triplet_loss(inputs, dist='sqeuclidean', margin='maxplus'):
     anchor, positive, negative = inputs
@@ -133,6 +55,24 @@ def triplet_loss_np(inputs, dist='sqeuclidean', margin='maxplus'):
         loss = np.log(1 + np.exp(loss))
     return np.mean(loss)
 
+def check_loss():
+    batch_size = 10
+    shape = (batch_size, 4096)
+
+    p1 = normalize(np.random.random(shape))
+    n = normalize(np.random.random(shape))
+    p2 = normalize(np.random.random(shape))
+    
+    input_tensor = [K.variable(p1), K.variable(n), K.variable(p2)]
+    out1 = K.eval(triplet_loss(input_tensor))
+    input_np = [p1, n, p2]
+    out2 = triplet_loss_np(input_np)
+
+    assert out1.shape == out2.shape
+    print(np.linalg.norm(out1))
+    print(np.linalg.norm(out2))
+    print(np.linalg.norm(out1-out2))
+    
 def GetModel():
     base_model = ResNet50(weights='imagenet', include_top=False, pooling='max')
     for layer in base_model.layers:
@@ -171,7 +111,12 @@ checkpoint = ModelCheckpoint(path_model, monitor='loss', verbose=1, save_best_on
 early = EarlyStopping(monitor="val_loss", mode="min", patience=2)
 callbacks_list = [checkpoint, early]  # early
 
- 
+def ShowImg(img):
+    plt.figure()
+    plt.imshow(img.astype('uint8'))
+    plt.show()
+    plt.close()
+    
 batch = next(gen_tr)
 
 img = batch[0]['anchor_input'][0]
